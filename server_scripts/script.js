adf500889361a549223eb774847b34d7124b9a3e
Zygorg <43007952+OfficialZygorg@users.noreply.github.com>
2021-12-05 11:23:56 +0100

    changed settings.logRemovedRecipes = (true)

--- server_scripts/script.js
+++ server_scripts/script.js
@@ -2,7 +2,7 @@
 
 settings.logAddedRecipes = true
 settings.logRemovedRecipes = true
-settings.logSkippedRecipes = false
+settings.logSkippedRecipes = true
 settings.logErroringRecipes = true
 
 console.info('Hello, World! (You will see this line every time server resources reload)')
